0ac561a44646e44e558f51c3ad88394efac49942
Verla Leida <vleida@si-e3.kzn.21-school.ru>
2021-08-19 11:35:58 +0300

    correct parcer

--- srcs/s_parser.c
+++ srcs/s_parser.c
@@ -1,17 +1,41 @@
 #include "head_minishell.h"
 
-static char 	*ft_dollar(t_data *data, char *str, int *i)
+static char 	*ft_merge_q1(char *str, int *i, char *rez)
 {
-	char	*rez;
+	char	*tmp;
+	int		j;
+
+	j = *i + 1;
+	while (str[j] != '\'')
+	{
+		if (!str[j])
+			return (NULL);
+		j++;
+	}
+	if (!rez)
+		tmp = ft_strndup(str, *i);
+	tmp = ft_strjoin_m(tmp, ft_strndup(&str[*i + 1], j - *i - 1), 3);
+	j++;
+	*i = j;
+	while (str[j] && str[j] != '\'' && str[j] != '\"' && str[j] != '$')
+		j++;
+	tmp = ft_strjoin_m(tmp, ft_strndup(&str[*i], j - *i), 3);
+	*i = j - 1;
+	return (tmp);
+}
+
+static char 	*ft_dollar(t_data *data, char *str, int *i, char *rez)
+{
 	char	*dol;
-	size_t	len;
+	size_t	j;
 	t_env 	*buf;
 
-	rez = ft_strndup(str, *i);
-	len = ft_strlen_m(&str[*i], ' ');
-	if (len > ft_strlen_m(&str[*i], '\"') || !len)
-		len = ft_strlen_m(&str[*i], '\"');
-	dol = ft_strndup(&str[*i + 1], len - 1);
+	j = *i;
+	if (!rez)
+		rez = ft_strndup(str, *i);
+	while (str[j] && str[j] != '\"' && str[j] != ' ')
+		j++;
+	dol = ft_strndup(&str[*i + 1], j - *i - 1);
 	buf = data->beg_env;
 	while (buf)
 	{
@@ -21,75 +45,58 @@ static char 	*ft_dollar(t_data *data, char *str, int *i)
 	}
 	if (buf)
 		rez = ft_strjoin_m(rez, buf->val, 1);
-	rez = ft_strjoin_m(rez, &str[*i + len], 1);
-	free(dol);
-	if (str)
-		free(str), str = NULL;
+	free(dol), *i = j;
+	while (str[j] && str[j] != '\'' && str[j] != '\"' && str[j] != '$')
+		j++;
+	rez = ft_strjoin_m(rez, ft_strndup(&str[*i], j - *i), 3);
+	*i = j - 1;
 	return (rez);
 }
 
-static char 	*ft_merge_q1(char *str, int *i)
+static char 	*ft_merge_q2(t_data *data, char *str, int *i, char *rez)
 {
 	int		j;
-	char	*tmp;
-	char	*tmp2;
-	char	*tmp3;
 
-	j = *i;
-	while (str[++(*i)])
-		if (str[*i] == '\'')
-			break ;
-	tmp = ft_substr(str, 0, j);
-	tmp2 = ft_substr(str, j + 1, *i - j - 1);
-	tmp3 = ft_strdup(str + *i + 1);
-	tmp = ft_strjoin_m(tmp, tmp2, 1);
-	tmp = ft_strjoin_m(tmp, tmp3, 1);
-	free(tmp2), free(tmp3);
-	if (str)
-		free(str), str = NULL;
-	return (tmp);
-}
-
-static char 	*ft_merge_q2(t_data *data, char *str, int *i)
-{
-	int		j;
-	char 	*rez;
-
-	(*i)++;
-	j = *i;
-	while (str[*i] != '\"')
+	if (!rez)
+		rez = ft_strndup(str, *i);
+	j = *i + 1;
+	while (str[j] && str[j] != '\"')
 	{
-		if (!str[*i])
-			v_pr_error("Error: qoute error\n", -2);
-		if (str[*i] == '$')
-			str = ft_dollar(data, str, i);
-		(*i)++;
+		if (str[j] == '$')
+		{
+			rez = ft_strjoin_m(rez, ft_strndup(&str[*i + 1], j - *i - 1), 3);
+			rez = ft_dollar(data, str, &j, rez);
+			*i = j;
+		}
+		j++;
 	}
-	rez = ft_strjoin_m(ft_strndup(str, j - 1),
-					   ft_strndup(&str[j], *i - j),3);
-	rez = ft_strjoin_m(rez, ft_strdup(&str[*i + 1]), 3);
-	if (str)
-		free(str), str = NULL;
+	rez = ft_strjoin_m(rez, ft_strndup(&str[*i + 1], j - *i - 1), 3);
+	j++, *i = j;
+	while (str[j] && str[j] != '\'' && str[j] != '\"' && str[j] != '$')
+		j++;
+	rez = ft_strjoin_m(rez, ft_strndup(&str[*i], j - *i), 3);
+	*i = j - 1;
 	return (rez);
 }
 
 int ft_parsing(t_data *data, char *str)
 {
-	int i;
+	int		i;
+	char	*rez;
 
 	i = 0;
 	while (str && str[i])
 	{
 		if (str[i] == '\'')
-			str = ft_merge_q1(str, &i);
+			rez = ft_merge_q1(str, &i, rez);
 		if (str[i] == '$')
-			str = ft_dollar(data, str, &i);
+			rez = ft_dollar(data, str, &i, rez);
 		if (str[i] == '\"')
-			str = ft_merge_q2(data, str, &i);
+			rez = ft_merge_q2(data, str, &i, rez);
 		i++;
 	}
-	if (!str)
+	if (!rez)
 		return (1);
-	data->str_cmd = str;
+	data->str_cmd = rez;
 	return (0);
 }
