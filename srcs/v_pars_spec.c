ff8615f4eb017f3886f2656aaa99453225f86acc
Verla Leida <vleida@si-b1.kzn.21-school.ru>
2021-08-23 16:57:11 +0300

    spec start

--- srcs/v_pars_spec.c
+++ srcs/v_pars_spec.c
@@ -1,8 +1,27 @@
 #include "head_minishell.h"
 
+int	ft_append_file(t_data *data, char *str, int *i)
+{
+	int		j;
+	char	*f_name;
+
+	if (data->fd_out >= 0)
+		close(data->fd_out);
+	while (str[*i] && &str[*i] == ' ')
+		*i = *i + 1;
+	j = *i;
+	while (str[j] && ft_ch_for_coinc(str[j], " |&;"))
+		j++;
+	if (j == *i)
+	{
+		v_pr_error(ERR_SH_NEWL, 0, str[*i], 1);
+		return (-2);
+	}
+}
+
 int	ft_open_app_here(t_data *data, char *str, int *i, char *rez)
 {
-	int rez;
+	int otv;
 
 	if (ft_ch_for_coinc(str[*i + 2], ";><"))
 	{
@@ -14,27 +33,39 @@ int	ft_open_app_here(t_data *data, char *str, int *i, char *rez)
 		v_pr_error(ERR_SH_NEWL, 0, str[*i + 2], 1);
 		return (-2);
 	}
-		// code bash: syntax error near unexpected token `newline'						return (-2); warning " |&"
+	if (ft_ch_for_coinc(str[*i + 2], " |&"))
+		// ??? " |&"																	return (-3); warning " |&"
 	if (str[*i] == '>' && str[*i + 1] == '>')
-		// code write create append in file												return (0);	незабудь кавычки, экранирование и $
+	{
+		*i = *i + 2;																	//return (0) or (-4);	незабудь кавычки, экранирование и $ 
+		return (ft_append_file(data, str, i));
+	}
 	else if (str[*i] == '<' && str[*i + 1] == '<')
-		// code here_doc																return (0);	незабудь кавычки, экранирование и $ ?
+		// code here_doc																return (0) or (-5);	незабудь кавычки, экранирование и $ ?
 	else
-		// code bash: syntax error near unexpected token `< or > or something wrong'	return (-3);
+		// code bash: syntax error near unexpected token `< or > or something wrong'	return (-6);
 }
 
 int	ft_open_file(t_data *data, char *str, int *i, char *rez)
 {
-	if (ft_ch_for_coinc(str[*i + 1], ";")
-		// code bash: syntax error near unexpected token `; or | or &'		return (-1);
-	if (ft_ch_for_coinc(str[*i + 1], "&| \0\n"))
-		// code bash: syntax error near unexpected token `newline'			return (-2);
+	if (ft_ch_for_coinc(str[*i + 1], ";"))
+	{
+		v_pr_error(ERR_SH_TKN, 0, str[*i + 2], 1);
+		return (-1);
+	}
+	if (ft_ch_for_coinc(str[*i + 1], "\0\n"))
+	{
+		v_pr_error(ERR_SH_NEWL, 0, str[*i + 2], 1);
+		return (-2);
+	}
+	if (ft_ch_for_coinc(str[*i + 2], " |&"))
+		// ??? " |&"														return (-3); warning " |&"
 	if (str[*i] == '>')
-		// code write creat trunc in file									return (0);	незабудь кавычки, экранирование и $
+		// code write creat trunc in file									return (0) or (-4);	незабудь кавычки, экранирование и $
 	else if (str[*i] == '<')
-		// code read from file												return (0);	незабудь кавычки, экранирование и $
+		// code read from file												return (0) or (-5);	незабудь кавычки, экранирование и $ ?
 	else
-		// error something wrong											return (-10);
+		// error something wrong											return (-7);
 }
 
 char	*ft_pool_cmd(t_data *data, char *str, int *i, char *rez)
